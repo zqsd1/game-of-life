3011098ad37729c0edc2640543c3d096a1e2cd38
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-10-31 17:57:04 +0100

    setup project and test cli

---
+++ GameOfLife/main.py
@@ -0,0 +1,12 @@
+def yourname():
+    print("what is your name ?")
+    name = input()
+    print(f"your name is {name}")
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
